c013a37cfee908328a3bb043d6b79545a2313382
skgupta89 <skgupta8909@gmail.com>
2022-05-26 07:08:45 +0530

    css added to form

--- src/TestFour.js
+++ src/TestFour.js
@@ -5,7 +5,6 @@ function TestFour() {
 
     const [text, settext] = useState('');
     const [file, setfile] = useState('');
-
 useEffect(()=>{
     (async ()=>{
         await openDB('DnD',1,{
@@ -36,15 +35,16 @@ e.preventDefault();
 
 const onImageChange=(e)=>{
     const file = e.target.files;
-    setfile(URL.createObjectURL(file[0]));
+    setfile(file);
 }
+
 
 
   return (
  <>
   <div>
-      <div style={{ width: "150px ", height:"250px", padding: "5px" }}>
-        <form>
+      <div style={{ width: "150px ", height:"250px", padding: "5px",margin:'15px' }}>
+        <form style={{width:'100%',height:'160px',borderRadius:'5px',border:'2px dashed', color:'red' , fontSize:'0.9rem', fontWeight:'500', display:'flex', justifyContent:'center', alignItem:'center',flexDirection:'column'}}>
           <input
             type="text"
             value={text}
@@ -56,7 +56,7 @@ const onImageChange=(e)=>{
             <input
               style={{
                 backgroundColor: "lightGrey",
-                height: "10%",
+                height: "60px",
                 width: "100%",
               }}
               type="file"
@@ -67,13 +67,13 @@ const onImageChange=(e)=>{
           </span>
           <br />
           <br />
-          <button onClick={addData}>add data</button>
+          <button style={{marginTop:'15px'}} onClick={addData}>add data</button>
         </form>
       </div>
 
       {/* SHOW DATA DIV */}
 
-      <div>
+      <div style={{ width:"100%", display:'flex', justifyContent:'flex-start',alignItem:'flex-start', flexWrap:'wrap', position:'relative', height:'auto', marginTop:'20px', maxHeight:'300px', overflowY:'auto'}}>
         <img src="" alt="img" width="100" height="50" />
       </div>
 
