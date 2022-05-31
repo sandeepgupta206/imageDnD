2531b12901e86125f8f99b5b87fc6bb01f69f56a
Sandeep Gupta <sandeepg@Cemtrexlabs.com>
2022-05-31 10:27:49 +0530

    past commit

--- src/TestFour.js
+++ src/TestFour.js
@@ -5,6 +5,7 @@ function TestFour() {
 
     const [text, settext] = useState('');
     const [file, setfile] = useState('');
+    let db;
 useEffect(()=>{
     (async ()=>{
         await openDB('DnD',1,{
@@ -14,21 +15,89 @@ useEffect(()=>{
                 })
             }
         });
+
+
+
+
+
+
+
+
+
+
+        // let trans = db.transaction('imgStore','readonly');
+        // let store = trans.objectStore('imgStore');
+        // let getReg = store.getAll();
+        // console.log(getReg);
+        // getReg.onsuccess = e=>{
+        //   console.log(e.targte);
+        // }
+        // getReg.onerror = e=>{
+        //   console.log(e);
+        // }
+
+        
+        // const request = indexedDB.open('DnD');
+        // request.onsuccess = ()=>{
+        //   const db = request.result;
+        //   console.log(db);
+        //   const store = db.transaction('imgStore', 'readonly');
+        //   let getReq = store.result;
+        // }
+        
+
+        // var transaction = db.transaction(["imgStore"]);
+        // var objectStore = transaction.objectStore("imgStore");
+        // var request = objectStore.get("0");
+        // request.onerror = event => {
+         
+        // };
+        // request.onsuccess = event => {
+        //   // Do something with the request.result!
+        //   console.log("Name for SSN 444-44-4444 is " + request.result.name);
+        // };
+
+            
     })();
-},[]);
+
+});
+
+
+// function getData(db,id){
+//   const txt = db.transaction('imgStore','readonly');
+//   const store = txt.objectStore('imgStore');
+
+//   let query = store.get(id);
+//   query.onsuccess = (event)=>{
+//     if(!event.target.result){
+//       console.log(`${id} not found`)
+//     }
+//     else{
+//       console.table(event.targte.result);
+//     }
+//   }
+//   query.onerror = event=>{
+//     console.log(event.target.errorCode);
+//   };
+
+//   txt.oncomplete = ()=>{
+//     db.close();
+//   };
+// };
+
+
+
+
 
 const addData =(e)=>{
 e.preventDefault();
 (async()=>{
     const db =await openDB('DnD',1);
-    console.log(db);
     const tx = db.transaction('imgStore', 'readwrite');
-    console.log(tx);
     const store =  tx.objectStore('imgStore')
-    console.log(store);
      store.put({title: text,image:file});
     await tx.done;
-    // console.log(store);
+    console.log(store);
     // console.log(store);
 })();
 }
@@ -40,10 +109,12 @@ const onImageChange=(e)=>{
 
 
 
+
+
   return (
  <>
   <div>
-      <div style={{ width: "150px ", height:"250px", padding: "5px",margin:'15px' }}>
+      <div style={{ width: "250px ", height:"250px", padding: "5px",margin:'30px' }}>
         <form style={{width:'100%',height:'160px',borderRadius:'5px',border:'2px dashed', color:'red' , fontSize:'0.9rem', fontWeight:'500', display:'flex', justifyContent:'center', alignItem:'center',flexDirection:'column'}}>
           <input
             type="text"
@@ -52,7 +123,7 @@ const onImageChange=(e)=>{
             placeholder="Enter Image lable"
           ></input>
          
-          <span style={{ backgroundColor: "lightGrey", padding: "2%" ,marginTop:'20px'}}>
+          <span style={{ backgroundColor: "lightGrey", padding: "2%" ,marginTop:'25px'}}>
             <input
               style={{
                 backgroundColor: "lightGrey",
